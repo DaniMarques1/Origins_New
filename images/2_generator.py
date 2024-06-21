5147257bd6a99b8024a18db521b8c460e58714ef
Daniel Marques <117253804+DaniMarques1@users.noreply.github.com>
2024-06-21 10:24:07 -0300

    wut

--- images/2_generator.py
+++ images/2_generator.py
@@ -4,52 +4,39 @@ import io
 import pandas as pd
 import os
 
-# Create the cache directory if it doesn't exist
 cache_directory = "image_cache"
+
 os.makedirs(cache_directory, exist_ok=True)
 
-# Read the CSV file
 df = pd.read_csv("Erc1155_info.csv")
 
-# Iterate through each row in the DataFrame
 for index, row in df.iterrows():
     image_url = row['Image URL']
     id_value = row['ID']
     file_name = f"{id_value}.png"
     file_path = os.path.join(cache_directory, file_name)
 
-    try:
-        if not os.path.exists(file_path):
-            # If the image is not in the cache, download and save it
-            response = requests.get(image_url)
-            if response.status_code == 200:
-                image = Image.open(io.BytesIO(response.content)).convert("RGBA")
-                image.save(file_path)
-                print(f"Image file {file_name} downloaded and cached.")
-            else:
-                print(f"Failed to download image from {image_url}")
-                continue
-        else:
-            print(f"Image file {file_name} already exists in the cache.")
+    if not os.path.exists(file_path):
+        # If the image is not in the cache, download and save it
+        response = requests.get(image_url)
+        image = Image.open(io.BytesIO(response.content))
+        image.save(file_path)
+        print(f"Image file {file_name} downloaded and cached.")
+    else:
+        # If the image is in the cache, load it from there
+        image = Image.open(file_path)
+        print(f"Image file {file_name} loaded from cache.")
 
-        # Now, open the image from the newly saved file
-        image = Image.open(file_path).convert("RGBA")
+    # Create the canvas
+    canvas = Image.new('RGBA', (500, 500), (0, 0, 0, 0))
 
-        # Create the canvas
-        canvas = Image.new('RGBA', (500, 500), (0, 0, 0, 0))
+    # Calculate the position for the center-left alignment
+    center_left_x = 0
+    center_left_y = (canvas.height - image.height) // 2
+    position = (center_left_x, center_left_y)
 
-        # Calculate the position for the center-left alignment
-        center_left_x = 0
-        center_left_y = (canvas.height - image.height) // 2
-        position = (center_left_x, center_left_y)
+    canvas.paste(image, position, mask=image)
 
-        canvas.paste(image, position, mask=image)
+    canvas.save(file_name)
 
-        output_file_path = os.path.join(cache_directory, f"output_{file_name}")
-        canvas.save(output_file_path)
-        print(f"Output image file {output_file_path} created successfully.")
-
-    except Exception as e:
-        print(f"An error occurred while processing {file_name}: {e}")
-
-print("All image files processed successfully.")
+print("All image files created successfully.")
